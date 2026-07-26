0c26f0b35243c6ad40b48b2d6c877de3dce26dd4
Patr1k66 <tim254202@gmail.com>
2026-07-26 22:09:22 +0300

    Update gallery.js

--- data/gallery.js
+++ data/gallery.js
@@ -14,7 +14,34 @@ const GALLERY = [
     alt: 'Инструменты и рабочее место мастера',
     caption: 'Профессиональный инструмент для точного ремонта'
   },
+  {const GALLERY = [
   {
+    src: 'assets/images/workshop.png',
+    alt: 'Ремонтный бокс с подъёмниками',
+    caption: 'Профессиональные подъёмники для любых работ'
+  },
+  {
+    src: 'assets/images/workshop.png',
+    alt: 'Оборудование автосервиса NORDBERG и ROSSVIK',
+    caption: 'Современное оборудование ведущих производителей'
+  },
+  {
+    src: 'assets/images/workshop.png',
+    alt: 'Просторная мастерская',
+    caption: 'Просторные и светлые ремонтные боксы'
+  },
+  {
+    src: 'assets/images/workshop.png',
+    alt: 'Чистое рабочее пространство',
+    caption: 'Чистота и порядок на каждом рабочем месте'
+  },
+  {
+    src: 'assets/images/workshop.png',
+    alt: 'Профессиональный автосервис ПОРШЕНЬ',
+    caption: 'Мастерская с полным циклом обслуживания'
+  }
+];
+
     src: 'assets/images/workshop/workshop-4.svg',
     alt: 'Зона шиномонтажа',
     caption: 'Современное оборудование для шиномонтажа'
