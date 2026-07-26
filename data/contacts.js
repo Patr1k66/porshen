0b6fa215a5dade39302081949ed9508dfd733991
Patr1k66 <tim254202@gmail.com>
2026-07-26 22:28:34 +0300

    Add files via upload

---
+++ data/contacts.js
@@ -0,0 +1,12 @@
+const CONTACTS = {
+  phone: '+7 (495) 123-45-67',
+  phoneHref: 'tel:+74951234567',
+  email: 'info@porshen.ru',
+  emailHref: 'mailto:info@porshen.ru',
+  address: 'г. Москва, ул. Автомоторная, д. 12',
+  social: [
+    { name: 'ВКонтакте', href: 'https://vk.com/', icon: 'vk' },
+    { name: 'Telegram', href: 'https://t.me/', icon: 'telegram' },
+    { name: 'WhatsApp', href: 'https://wa.me/74951234567', icon: 'whatsapp' }
+  ]
+};
